977c9c1006fdcc03c3212d34d6476653b345a85f
Pratham Gupta <prathamgupta.wk@gmail.com>
2026-01-30 23:15:25 +0530

    all uploading work

--- server/src/controller/uploadMovie.ts
+++ server/src/controller/uploadMovie.ts
@@ -10,6 +10,7 @@ import cloudinary from '../config/cloudinaryConfig.js';
 import moviePoster from '../model/moviePoster.js';
 import fs from 'fs';
 import movie from '../model/movie.js';
+import movieTrailer from '../model/movieTrailer.js';
 
 export async function uploadMovieAbout(req: Request, res: Response) {
     const userId = req.user;
@@ -171,6 +172,60 @@ export async function uploadMovieVideo(req: Request, res: Response) {
             }
         });
         return res.json({ message: 'Movie Video Uploaded' });
+    }
+    return res.status(500).json({ message: 'Somethin went work in Upload' });
+}
+
+export async function uploadMovieTrailer(req: Request, res: Response) {
+    const userId = req.user;
+    const headers = req.headers.authorization;
+    if (!headers) {
+        return res.status(400).json({ message: 'headers not found' });
+    }
+    const token = headers.split(' ')[1];
+    if (!token) {
+        return res.status(400).json({ message: 'invalid format of token' });
+    }
+    let movieAboutId = '';
+    try {
+        const tokenData = jwt.verify(
+            token,
+            env.MOVIEVIDEOTOKEN,
+        ) as jwt.JwtPayload;
+        movieAboutId = tokenData.movieAboutId;
+        if (!movieAboutId) {
+            return res
+                .status(500)
+                .json({ message: 'Somethin went work in token' });
+        }
+        const movieAboutData = await movieAbout.findById(movieAboutId);
+        if (movieAboutData && !movieAboutData.owner.equals(userId)) {
+            return res
+                .status(403)
+                .json({ message: 'only owner can upload Movie Video' });
+        }
+    } catch (error) {
+        return res.status(400).json({ message: 'corrupted token' });
+    }
+    if (req.file?.path) {
+        const uploadData = await cloudinary.uploader.upload(req.file.path, {
+            resource_type: 'video',
+            use_filename: true,
+            unique_filename: false,
+            folder: 'Turtle_Movies_Trailers',
+            overwrite: true,
+        });
+        const newMovie = new movieTrailer({
+            about: movieAboutId,
+            movieTrailerUrl: uploadData.secure_url,
+        });
+        await newMovie.save();
+        fs.unlink(req.file.path, (err) => {
+            if (err) {
+                console.log('Cannot delete the uploadMovie');
+            }
+        });
+        return res.json({ message: 'Movie Trailer Uploaded' });
     }
     return res.status(500).json({ message: 'Somethin went work in Upload' });
 }
--- server/src/routes/uploadMovie.ts
+++ server/src/routes/uploadMovie.ts
@@ -1,12 +1,39 @@
 import express from 'express';
-import { uploadMovieAbout, uploadMovieVideo, uploadPoster } from '../controller/uploadMovie.js';
+import {
+    uploadMovieAbout,
+    uploadMovieTrailer,
+    uploadMovieVideo,
+    uploadPoster,
+} from '../controller/uploadMovie.js';
 import upload from '../middleware/multerUpload.js';
-import { fileValidation, videoValidation } from '../middleware/fileValidation.js';
+import {
+    fileValidation,
+    videoValidation,
+} from '../middleware/fileValidation.js';
 import { userInfo } from '../middleware/userInfo.js';
 const router = express.Router();
 
-router.post('/uploadMovieAbout',userInfo,uploadMovieAbout);
-router.post('/uploadPoster',userInfo,upload.single('file'),fileValidation,uploadPoster)
-router.post('/uploadMovie',userInfo,upload.single('file'),videoValidation,uploadMovieVideo)
+router.post('/uploadMovieAbout', userInfo, uploadMovieAbout);
+router.post(
+    '/uploadPoster',
+    userInfo,
+    upload.single('file'),
+    fileValidation,
+    uploadPoster,
+);
+router.post(
+    '/uploadMovie',
+    userInfo,
+    upload.single('file'),
+    videoValidation,
+    uploadMovieVideo,
+);
+router.post(
+    '/uploadTrailer',
+    userInfo,
+    upload.single('file'),
+    videoValidation,
+    uploadMovieTrailer,
+);
 
 export default router;
