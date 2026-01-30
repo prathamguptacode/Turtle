b36b6a6fe424ae52987113879da67aecec059fc3
Pratham Gupta <prathamgupta.wk@gmail.com>
2026-01-30 18:37:04 +0530

    movie upload changes

--- server/src/routes/uploadMovie.ts
+++ server/src/routes/uploadMovie.ts
@@ -4,6 +4,6 @@ import upload from '../middleware/multerUpload.js';
 import { fileValidation } from '../middleware/fileValidation.js';
 const router = express.Router();
 
-router.post('/uploadMovie', upload.single('file'), fileValidation ,uploadMovieAbout);
+router.post('/uploadMovieAbout',uploadMovieAbout);
 
 export default router;
